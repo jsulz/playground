a4f6d4d6956516f11470760ca57b8796d64d6492
Jared Sulzdorf <j.sulzdorf@gmail.com>
2023-12-05 17:56:51 -0800

    doing a bunch of setup

--- playground/playground.py
+++ playground/playground.py
@@ -6,4 +6,14 @@ play = Blueprint("playground", __name__)
 
 @play.route("/")
 def home():
-    return render_template("base.html.jinja")
+    return render_template("home.html.jinja")
+
+
+@play.route("/about")
+def about():
+    return render_template("about.html.jinja")
+
+
+@play.route("/contact")
+def contact():
+    return render_template("contact.html.jinja")
