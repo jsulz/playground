11b429d9ca87888e5f91187a5b183f517fc902b5
Jared Sulzdorf <j.sulzdorf@gmail.com>
2023-12-17 15:38:30 -0800

    most of pig latin is done

--- playground/app.py
+++ playground/app.py
@@ -4,6 +4,7 @@ from . import birthday_paradox
 from . import factor_finder
 from . import dice_roll_simulator
 from . import numbers
+from . import pig_latin_encoder
 
 
 def create_app(test_config=None):
@@ -15,6 +16,7 @@ def create_app(test_config=None):
     app.register_blueprint(factor_finder.ff)
     app.register_blueprint(dice_roll_simulator.drs)
     app.register_blueprint(numbers.nsc)
+    app.register_blueprint(pig_latin_encoder.ple)
 
     app.register_error_handler(404, page_not_found)
 
---
+++ playground/pig_latin_encoder.py
@@ -0,0 +1,56 @@
+from flask import Blueprint, render_template, request
+
+ple = Blueprint("pig-latin-encoder", __name__, template_folder="templates")
+
+
+@ple.route("/pig-latin-encoder", methods=["GET", "POST"])
+def pig_latin_encoder():
+    if request.method == "POST":
+        results = {}
+        punctuation = {"(", ")", ".", ",", "!", "?", ":", ";", "'", '"'}
+        vowels = {"a", "e", "i", "o", "u", "y"}
+        # Take the string and split it on ' '
+        message = request.form["message"].split(" ")
+        for i, word in enumerate(message):
+            if word.isdigit():
+                results["error"] = True
+                return render_template(
+                    "bbospp/piglatinencoder.html.jinja", results=results
+                )
+
+            # for each word, first check to see if the end is a puncuation
+            # if it is, count how many letters are puncuation and ignore them
+            j = -1
+            prefix = word
+            suffix = ""
+            if word[j] in punctuation:
+                while word[j] in punctuation:
+                    j -= 1
+                j += 1
+                prefix = word[:j]
+                suffix = word[j:]
+            # If a word starts with a vowel, just add "way" to the end
+            if prefix[0].lower() in vowels:
+                prefix += "way"
+                message[i] = prefix + suffix
+            # If a word starts with a constanant, find the number of constanatns
+            # that a word starts with and move them to the end of the word and append with "ay"
+            # rejoin the string and return it
+            else:
+                k = 0
+                while prefix[k].lower() not in vowels:
+                    k += 1
+
+                titlecase = prefix.istitle()
+
+                if titlecase:
+                    new_word = prefix[k:].title() + prefix[:k].lower() + "ay"
+                else:
+                    new_word = prefix[k:] + prefix[:k] + "ay"
+
+                message[i] = new_word + suffix
+
+        results["encoded"] = " ".join(message)
+
+        return render_template("bbospp/piglatinencoder.html.jinja", results=results)
+    return render_template("bbospp/piglatinencoder.html.jinja")
