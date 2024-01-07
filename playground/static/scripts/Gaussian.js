0713a244796bca0a82e846ff847e67f096f2eb0e
Jared Sulzdorf <j.sulzdorf@gmail.com>
2024-01-06 16:26:03 -0800

    done with gaussian pdf

--- playground/static/scripts/Gaussian.js
+++ playground/static/scripts/Gaussian.js
@@ -2,33 +2,50 @@ import React, { useState } from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 export default function Gaussian() {
-    let arr = data();
-    console.log(arr);
+    const [gauss, setGauss] = useState({mu: 10, sigma: 2});
+    const data = gauss_pdf(gauss["mu"], gauss["sigma"]);
+
     return (
-      <LineChart width={800} height={600} data={arr}>
-            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-            <CartesianGrid stroke="#ccc" />
-            <XAxis />
-            <YAxis />
-      </LineChart>
+      <>
+        <LineCreate newLine={setGauss}/>
+        <LineChart width={800} height={600} data={data}>
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc"/>
+                <XAxis dataKey="val" tickFormatter={(value) => value.toFixed(2)}/>
+                <YAxis/>
+        </LineChart>
+      </>
     );
 }
 
-const data = () => {
+const LineCreate = ({newLine}) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formJson = Object.fromEntries(formData.entries());
+        newLine({mu: parseFloat(formJson.mu), sigma: parseFloat(formJson.sigma)});
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="mu" id="mu" required/>
+            <input type="text" name="sigma" id="sigma" required/>
+            <button type="submit" className="col-3 me-2  btn btn-primary">Create Line</button>
+        </form>
+    );
+}
+
+const gauss_pdf = (mu, sigma) => {
 
     const x = [];
-    for (let i = 0; i < 10; i += .1){
+    const for_start = mu - 4*sigma;
+    const for_stop = mu + 4*sigma;
+    for (let i = for_start; i < for_stop; i += (for_stop - for_start) / 50){
         // sigma is 2
-        const sigma = 2
-        const mu = 10
-        let a = 1 / (sigma * Math.sqrt(2 * Math.PI) )
-        let b = -(1/2) * Math.pow( (i - mu / sigma ), 2)
-        let f_x = a * Math.pow(Math.E, b)
+        let a = 1.0 / (sigma * (Math.sqrt(2.0 * Math.PI)) );
+        let b = -(1/2) * Math.pow( ( (i - mu) / sigma ), 2.0);
+        let f_x =(a * Math.pow(Math.E, b));
 
-        x.push({uv: f_x });
+        x.push({val: i, uv: f_x });
     }
-    if(x.length == 101){
-        return x
-    }
-    console.log(x)
+    return x;
 }
