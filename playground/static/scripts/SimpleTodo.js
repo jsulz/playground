b94b06485169037eeda43695aa89d21dff9b51e5
Jared Sulzdorf <j.sulzdorf@gmail.com>
2024-01-24 16:02:37 -0800

    form wired in for todo list

--- playground/static/scripts/SimpleTodo.js
+++ playground/static/scripts/SimpleTodo.js
@@ -1,5 +1,45 @@
 import React from "react";
 
 export default function SimpleTodo() {
-  return <h1>Hello</h1>;
+  const list = [{ title: "Thing 1", status: "todo" }];
+  return <AddItem />;
 }
+
+const AddItem = () => {
+  const handleSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
+  };
+  return (
+    <div className="row">
+      <form onSubmit={handleSubmit}>
+        <div className="col-6 mb-3">
+          <label className="form-label" for="task">
+            Task Name
+          </label>
+          <input type="text" id="task" name="task" className="form-control" />
+        </div>
+        <div className="col-6 mb3">
+          <button type="submit" className="col-3 me-2  btn btn-primary">
+            Add Task
+          </button>
+          <button
+            type="reset"
+            className="col-3 btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Delete List
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const TodoTable = () => {
+  return <h2>Hello</h2>;
+};
+
+const TodoItem = () => {
+  return <h2>World</h2>;
+};
