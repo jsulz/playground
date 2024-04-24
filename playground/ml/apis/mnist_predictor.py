fa9d606c94f3a461827dece8575f4d93e5120e3f
Jared Sulzdorf <j.sulzdorf@gmail.com>
2024-04-24 14:31:42 -0700

    test

--- playground/ml/apis/mnist_predictor.py
+++ playground/ml/apis/mnist_predictor.py
@@ -34,8 +34,12 @@ def predict():
     final = np.array(image, dtype=np.float32) / 255.0
 
     print(
-        os.path.abspath(
-            os.path.join(os.path.dirname(__file__), "../artifacts/mnist_model.keras")
+        os.path.exists(
+            os.path.abspath(
+                os.path.join(
+                    os.path.dirname(__file__), "../artifacts/mnist_model.keras"
+                )
+            )
         )
     )
 
